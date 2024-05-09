de718933f4a290eed3138d417310061c80366e35
unknown <pablohvz@gmail.com>
2024-05-09 14:17:12 -0300

    Prueba3 router

--- Router.php
+++ Router.php
@@ -18,48 +18,77 @@ class Router
 
     public function comprobarRutas()
     {
+        
+        // Proteger Rutas...
         session_start();
+
+        //Arreglo de rutas protegidas...
+        $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
+
         $auth = $_SESSION['login'] ?? null;
 
-        //Arreglo de rutas protegidas
-        $rutas_protegidas = [
-            '/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar',
-            '/vendedores', '/vendedores/actualizar', '/vendedores/eliminar',
-           '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'
-        ];
-
-        //prueba
+        //$currentUrl/urlActual = $_SERVER['PATH_INFO'] ?? '/'; //version desarrollo
         $urlActual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
-        // if (isset($_SERVER['PATH_INFO'])) {
-        //     //$urlActual = $_SERVER['PATH_INFO'];
-        //    $urlActual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
-            
-        // } else {
-        //     $urlActual = $_SERVER['REQUEST_URI'];
-        //     //debuguear($urlActual);
-        // }
-        //Fin prueba
+        $method = $_SERVER['REQUEST_METHOD'];
 
-        //original code : $urlActual = $_SERVER['PATH_INFO'];
-        $metodo = $_SERVER['REQUEST_METHOD'];
-
-        if ($metodo === 'GET') {
+        if ($method === 'GET') {
             $fn = $this->rutasGET[$urlActual] ?? null;
-        } else { //debuguear($_POST);
+        } else {
             $fn = $this->rutasPOST[$urlActual] ?? null;
         }
 
-        //Proteger las rutas
-        if (in_array($urlActual, $rutas_protegidas) && !$auth) {
-            header('Location: /');
-        }
-        if ($fn) {
-            //La url existe y hay una funcion asociada
-            call_user_func($fn, $this);
+
+        if ( $fn ) {
+            // Call user fn va a llamar una función cuando no sabemos cual sera
+            call_user_func($fn, $this); // This es para pasar argumentos
         } else {
-            echo "Pagina no encontrada";
+            echo "Página No Encontrada o Ruta no válida";
         }
     }
+    // public function comprobarRutas()
+    // {
+    //     session_start();
+    //     $auth = $_SESSION['login'] ?? null;
+
+    //     //Arreglo de rutas protegidas
+    //     $rutas_protegidas = [
+    //         '/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar',
+    //         '/vendedores', '/vendedores/actualizar', '/vendedores/eliminar',
+    //        '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'
+    //     ];
+
+    //     //prueba
+    //     $urlActual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
+    //     // if (isset($_SERVER['PATH_INFO'])) {
+    //     //     //$urlActual = $_SERVER['PATH_INFO'];
+    //     //    $urlActual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
+            
+    //     // } else {
+    //     //     $urlActual = $_SERVER['REQUEST_URI'];
+    //     //     //debuguear($urlActual);
+    //     // }
+    //     //Fin prueba
+
+    //     //original code : $urlActual = $_SERVER['PATH_INFO'];
+    //     $metodo = $_SERVER['REQUEST_METHOD'];
+
+    //     if ($metodo === 'GET') {
+    //         $fn = $this->rutasGET[$urlActual] ?? null;
+    //     } else { //debuguear($_POST);
+    //         $fn = $this->rutasPOST[$urlActual] ?? null;
+    //     }
+
+    //     //Proteger las rutas
+    //     if (in_array($urlActual, $rutas_protegidas) && !$auth) {
+    //         header('Location: /');
+    //     }
+    //     if ($fn) {
+    //         //La url existe y hay una funcion asociada
+    //         call_user_func($fn, $this);
+    //     } else {
+    //         echo "Pagina no encontrada";
+    //     }
+    // }
     //Muestra una vista
     public function render($view, $datos = [])
     {
